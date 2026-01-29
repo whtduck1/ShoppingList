b85064bd6e93005966f5459beced4448a0ad8f85
whtduck1 <myszormateusz02@gmail.com>
2026-01-30 00:37:12 +0100

    Dodano kontroler ShoppingController.cs z GET i  POST działa.

---
+++ backend/ShoppingList.Api/Controllers/ShoppingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingList.Api.Data;
+using ShoppingList.Api.Models;
+
+namespace ShoppingList.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ShoppingController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ShoppingItem>>> GetItems()
+        {
+            return await _context.ShoppingItems.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingItem>> AddItem(ShoppingItem item)
+        {
+            _context.ShoppingItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return Ok(item);
+        }
+
+    }
+}
