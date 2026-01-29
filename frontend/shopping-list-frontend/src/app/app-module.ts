6ff43d56110916e9b7d59902165d061faff73d2d
Oliwia Krzanowska <oliwia.krzanowska.ok@gmail.com>
2026-01-29 23:59:45 +0000

    Dodano interfejs listy zakupów i zmockowane dane.

--- frontend/shopping-list-frontend/src/app/app-module.ts
+++ frontend/shopping-list-frontend/src/app/app-module.ts
@@ -1,8 +1,9 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { FormsModule } from '@angular/forms';
+import { ShoppingListComponent } from './shopping-list.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ShoppingListComponent
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
